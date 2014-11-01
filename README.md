6738c609b61a8dbeedd66972ddbd54b23ae46b70
Dennis Hernández <dennishernandezvargas@gmail.com>
2014-11-01 14:10:24 -0600

    Update Readme and added travis status

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 bootstrap-table-flatJSON
 ========================
 
-Plugin to flat the JSON object
+[![Build Status](https://travis-ci.org/djhvscf/bootstrap-table-flatJSON.svg?branch=master)](https://travis-ci.org/djhvscf/bootstrap-table-flatJSON)
+
+Plugin to flat the JSON object. Extension for Bootstrap-table
--- src/bootstrap-table-flatJSON.js
+++ src/bootstrap-table-flatJSON.js
@@ -1,8 +1,37 @@
-/**
- * @author Dennis Hernández <djhv92@hotmail.com>
- * extensions: TODO
+/ /**
+ * bootstrap-table-flatJSON.js
+ * @version: v1.0.0
+ * @author: Dennis Hernández
+ * @webSite: http://djhvscf.github.io/Blog
+ *
+ * Created by Dennis Hernández on 01/Nov/2014.
+ *
+ * Copyright (c) 2014 Dennis Hernández http://djhvscf.github.io/Blog
+ *
+ * The MIT License (http://www.opensource.org/licenses/mit-license.php)
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
-
+ 
 (function ($) {
     'use strict';
 
